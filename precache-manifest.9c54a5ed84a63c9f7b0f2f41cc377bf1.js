b9461e8f7e59be5df38a9867ff793c59c3f8f0e6
Poyu Chen <23rdstride@gmail.com>
2019-07-30 15:13:36 +0800

    Updates

---
+++ precache-manifest.9c54a5ed84a63c9f7b0f2f41cc377bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ab91afc2386b5d16f4464bda86cd0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "9bed1f67cb98d160f9b5",
+    "url": "/robofriends/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "621909fed4d69c76b50e",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "9bed1f67cb98d160f9b5",
+    "url": "/robofriends/static/js/2.d0c5bfab.chunk.js"
+  },
+  {
+    "revision": "621909fed4d69c76b50e",
+    "url": "/robofriends/static/js/main.57e225c8.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
